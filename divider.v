caf1d2033ba28c06cb2563aa65b211ec98039f02
jrogge <jwrogge@live.com>
2017-12-10 05:00:05 -0600

    kay :w;

--- divider.v
+++ divider.v
@@ -1,9 +1,12 @@
 module clock_divider( input clkin, output clkout);
+    parameter
+        offset = 1;
+        
     reg [31:0] counter = 1;
     reg temp_clk = 0;
     always @ (posedge(clkin))
     begin
-        if (counter == 6)
+        if (counter == offset)
         begin
             counter <= 1;
             temp_clk <= ~temp_clk;
